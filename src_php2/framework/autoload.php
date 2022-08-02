43aa8a163016fcc08973f19cb1c90c22bb6e66e1
Boris Korobkov <boris@korobkov.su>
2022-08-02 23:45:40 +0200

    remove src_php2

--- src_php2/framework/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-spl_autoload_register(function ($className) {
-    $fileName = __DIR__ . '/../' . str_replace('\\', '/', $className) . '.php';
-    if (!file_exists($fileName)) {
-        throw new InvalidArgumentException('404. Not found');
-    }
-    require_once $fileName;
-});
